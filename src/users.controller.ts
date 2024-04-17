740208e53ac54eafd91d966d02d2576f249dc9bb
@Rinkesh_Epesign375 <rinkesh@epesign.com>
2024-04-18 00:15:17 +0530

    How to pass status code custom redirecting new route response custom changes0

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get,Post,Put,Patch,Delete, Req } from "@nestjs/common";
+import { Controller, Get,Post,Put,Patch,Delete, Req, HttpCode } from "@nestjs/common";
 import { Request } from "express";
 
 @Controller("/users")
 export class UsersController {
 
     @Get("/profile")
+    @HttpCode(200) //Even though nest-js by default pass the stauts code for get,post,put,patch,delete but you pass your custom httpcode too for get,put,patch etc;
     getProfile(@Req() req:Request){
         // console.log(req.params)
         return new Promise((resolve,reject)=>{
